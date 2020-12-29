e687a31af821b72e2ecf4c79485169588cf909e2
lord <marialindananono@gmail.com>
2020-12-29 06:09:10 +0300

    first commit

---
+++ config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'cluster0.eewys.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'my-project'),
+        username: env('DATABASE_USERNAME', 'lord'),
+        password: env('DATABASE_PASSWORD', 'phaneroo@5'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '025bd054-9780-4c56-9de8-775de9b52a42'
+};
